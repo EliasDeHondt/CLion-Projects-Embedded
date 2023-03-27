31a7bab5cbdd9560fdba86f41f18039102762060
EliasDeHondt <Elias.dehondt@outlook.com>
2023-03-27 20:10:46 +0200

    Commit

---
+++ W19P4/LED/LED_TEST.h
@@ -0,0 +1,14 @@
+//
+// Created by elias on 27/03/2023.
+//
+
+#ifndef CLION_PROJECTS_EMBEDDED_LED_TEST_H
+#define CLION_PROJECTS_EMBEDDED_LED_TEST_H
+
+
+class LED_TEST {
+
+};
+
+
+#endif //CLION_PROJECTS_EMBEDDED_LED_TEST_H

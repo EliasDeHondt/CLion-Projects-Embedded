4fcbdc73b48e008d7cc8f6e5fcde804cb7de85c1
EliasDeHondt <Elias.dehondt@outlook.com>
2023-03-28 19:39:04 +0200

    Commit

--- W19P4/HalloWorld/HalloWorld.cpp
+++ W19P4/HalloWorld/HalloWorld.cpp
@@ -3,6 +3,15 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, world!";
+    cout << "Hello, world!\n";
+
+
+    int num1, num2, sum;
+    cout << "Enter first number:";
+    cin >> num1;
+    cout << "Enter second number:";
+    cin >> num2;
+    sum = num1 + num2;
+    cout << "The sum of " << num1 << " and " << num2 << " is " << sum << endl;
     return 0;
 }

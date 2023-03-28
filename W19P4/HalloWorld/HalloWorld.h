06cc07750eb6ae7487939f038c73312280945e6c
EliasDeHondt <Elias.dehondt@outlook.com>
2023-03-28 19:32:32 +0200

    Commit

---
+++ W19P4/HalloWorld/HalloWorld.h
@@ -0,0 +1,14 @@
+//
+// Created by elias on 28/03/2023.
+//
+
+#ifndef CLION_PROJECTS_EMBEDDED_HALLOWORLD_H
+#define CLION_PROJECTS_EMBEDDED_HALLOWORLD_H
+
+
+class HalloWorld {
+
+};
+
+
+#endif //CLION_PROJECTS_EMBEDDED_HALLOWORLD_H

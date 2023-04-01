25ff197ade02821a57fe4812917ca2855a00a838
EliasDeHondt <Elias.dehondt@outlook.com>
2023-04-01 14:35:35 +0200

    Commit

--- W19P4/HalloWorld/HalloWorld.cpp
+++ W19P4/HalloWorld/HalloWorld.cpp
@@ -6,12 +6,16 @@ int main() {
     cout << "Hello, world!\n";
 
 
-    int num1, num2, sum;
-    cout << "Enter first number:";
-    cin >> num1;
-    cout << "Enter second number:";
-    cin >> num2;
-    sum = num1 + num2;
-    cout << "The sum of " << num1 << " and " << num2 << " is " << sum << endl;
+
+
+
+
+//    int num1, num2, sum;
+//    cout << "Enter first number:";
+//    cin >> num1;
+//    cout << "Enter second number:";
+//    cin >> num2;
+//    sum = num1 + num2;
+//    cout << "The sum of " << num1 << " and " << num2 << " is " << sum << endl;
     return 0;
 }
